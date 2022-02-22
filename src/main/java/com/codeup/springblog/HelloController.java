08b76e486a36a845c104865457ac6ed0a7a6614d
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-02-22 16:14:41 -0600

    reading/examples completed

--- src/main/java/com/codeup/springblog/HelloController.java
+++ src/main/java/com/codeup/springblog/HelloController.java
@@ -1,6 +1,7 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 //  let spring know we will have a req/res body
@@ -24,20 +25,38 @@ public class HelloController {
         return "test";
     }
 
-    //  use curly braces to establish Path Variables in the mapping definition
-    @GetMapping("/hello/{name}")
-    @ResponseBody
-    //  use annotation for Path Variable
-    //    If the path variable we are looking for is not a string, we can simply define the parameter with a different type.
-    public String helloName(@PathVariable String name){
-        return "<h1>Hello, " + name + "!</h1>";
-    }
+//    //  use curly braces to establish Path Variables in the mapping definition
+//    @GetMapping("/hello/{name}")
+//    @ResponseBody
+//    //  use annotation for Path Variable
+//    //    If the path variable we are looking for is not a string, we can simply define the parameter with a different type.
+//    public String helloName(@PathVariable String name){
+//        return "<h1>Hello, " + name + "!</h1>";
+//    }
 
     //  REQUEST MAPPING
     @RequestMapping(path = "/increment/{number}", method = RequestMethod.GET)
     @ResponseBody
     public String increment(@PathVariable int number){
         return number + " plus one is " + (number + 1) + "!";
+    }
+
+
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, Model model){
+        model.addAttribute("name", name);
+        return "hello";
+    }
+
+    @GetMapping("/join")
+    public String showJoinForm(){
+        return "join";
+    }
+
+    @PostMapping("/join")
+    public String joinCohort(@RequestParam(name = "cohort") String cohort, Model model){
+        model.addAttribute("cohort", "Welcome to " + cohort + "!");
+        return "join";
     }
 
 }
