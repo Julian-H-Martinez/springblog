ca7a5ec3421e133edadd5e4b40813241307cc94b
Julian-H-Martinez <julian.h.martinez10@gmail.com>
2022-03-02 09:52:35 -0600

    midway through lec; to be continued...

--- src/main/java/com/codeup/springblog/models/User.java
+++ src/main/java/com/codeup/springblog/models/User.java
@@ -59,6 +59,14 @@ public class User {
         this.posts = posts;
     }
 
+    //  use as alternative to clone
+    public User(User copy) {
+        id = copy.id; // This line is SUPER important! Many things won't work if it's absent
+        email = copy.email;
+        username = copy.username;
+        password = copy.password;
+    }
+
     //  GETTERS/SETTERS
 
     public long getId() {
